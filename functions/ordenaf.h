0f30480a6878ccec92d22ba0fc0c6eb31ee84a3a
black0ut <89255690+black0utzy@users.noreply.github.com>
2026-02-25 19:50:55 -0300

    Delete functions/ordenaf.h

--- functions/ordenaf.h
+++
@@ -1,6 +0,0 @@
-void troca(int a[], int x,int y);
-void bubbleSort(int a[],int n);
-void insertSort(int vetor[],int size);
-void selectionSort(int vetor[], int size);
-void mergeSort(int vetor[], int size);
-void quickSort(int vetor[], int size);
