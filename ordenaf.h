aa7793e5748c13fdd9bf66e55e915bd00980f3c7
black0ut <89255690+black0utzy@users.noreply.github.com>
2026-02-25 19:50:20 -0300

    Delete ordenaf.h

--- ordenaf.h
+++
@@ -1,5 +0,0 @@
-void troca(int a[], int x,int y);
-void bobbleSort(int a[],int n);
-void insertSort(int vetor[],int size);
-void margesort(int *ini, int *slow, int *fim);
-void marge(int *ini, int *fim); // usar função marge marge(vetor, vetor + tamanho - 1);
